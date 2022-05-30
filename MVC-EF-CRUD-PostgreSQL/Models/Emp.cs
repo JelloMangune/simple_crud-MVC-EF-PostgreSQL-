90f57d65acab48bb2b2d8b7984b034f629eabb5d
JelloMangune <enugnam.ollej@gmail.com>
2022-05-30 20:42:45 +0800

    Simple CRUD

---
+++ MVC-EF-CRUD-PostgreSQL/Models/Emp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_EF_CRUD_PostgreSQL.Models
+{
+    [Table("emp",Schema ="public")]
+    public class Emp
+    {
+        [Key]
+        public int empid { get; set; }
+        public string empname { get; set; }
+        public string empaddress { get; set; }
+        public string email { get; set; }
+        public int salary { get; set; }
+    }
+}
